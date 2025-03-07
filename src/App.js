c5d7b5b22d3d3a47cc3f8c9acc04ad580b1bd415
Navinya Dewamiththa <158686982+Navinya20@users.noreply.github.com>
2025-03-08 01:20:00 +0530

    connect mongoDB

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [name,setName] = useState('');
+  const [datetime,setDatetime] = useState('');
+  const[description,setDescription] = useState('');
+  function addNewTransaction(ev){
+    ev.preventDefault();
+    const url = process.env.REACT_APP_API_URL+'/transactions';
+    const price = name.split(' ')[0]; 
+    fetch(url,{
+      method: 'POST',
+      headers:{'Content-type':'application/json'},
+      body: JSON.stringify({
+        name: name.substring(price.length+1),
+        description,
+        datetime
+      })
+    }).then(response => {
+      response.json().then(json => {
+        console.log('result',json);
+      });
+    });
+  }
   return (
    <main>
     <h1>$400<span>.00</span></h1>
-    <form>
+    <form onSubmit={addNewTransaction}>
       <div className='basic'>
-        <input type="text" placeholder={'+200 new samsung tv'}/>
-        <input type="datetime-local"/>
+        <input  type="text" 
+                value={name} 
+                onChange={ev => setName(ev.target.value)}
+                placeholder={'+200 new samsung tv'}/>
+        <input  value={datetime}
+                onChange={ev => setDatetime(ev.target.value)}
+                type="datetime-local"/>
       </div>
       <div className="description">
-        <input type="text" placeholder={'description'}/>
+        <input  type="text" 
+                value={description}
+                onChange={ev => setDescription(ev.target.value)}
+                placeholder={'description'}/>
       </div> 
       <button type="submit">Add new transaction</button>
     </form>
